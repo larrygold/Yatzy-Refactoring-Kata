054ce79d5896acd6eb1fb0cd8741e79874d91c70
Larry Goldstein <larry@goldstein.fr>
2021-11-17 12:31:13 +0100

    Gros refacto : plus de méthode static

--- csharp/Yatzy.Test/YatzyTest.cs
+++ csharp/Yatzy.Test/YatzyTest.cs
@@ -8,32 +8,32 @@ namespace Yatzy.Test
         public void Chance_scores_sum_of_all_dice()
         {
             var expected = 15;
-            var actual = Yatzy.GetScoreChance(2, 3, 4, 5, 1);
+            var actual = new Yatzy(2, 3, 4, 5, 1).GetScoreChance(2, 3, 4, 5, 1);
             Assert.Equal(expected, actual);
-            Assert.Equal(16, Yatzy.GetScoreChance(3, 3, 4, 5, 1));
+            Assert.Equal(16, new Yatzy(3, 3, 4, 5, 1).GetScoreChance(3, 3, 4, 5, 1));
         }
 
         [Fact]
         public void Fact_1s()
         {
-            Assert.True(Yatzy.GetScoreOnes(1, 2, 3, 4, 5) == 1);
-            Assert.Equal(2, Yatzy.GetScoreOnes(1, 2, 1, 4, 5));
-            Assert.Equal(0, Yatzy.GetScoreOnes(6, 2, 2, 4, 5));
-            Assert.Equal(4, Yatzy.GetScoreOnes(1, 2, 1, 1, 1));
+            Assert.True(new Yatzy(1, 2, 3, 4, 5).GetScoreOnes(1, 2, 3, 4, 5) == 1);
+            Assert.Equal(2, new Yatzy(1, 2, 1, 4, 5).GetScoreOnes(1, 2, 1, 4, 5));
+            Assert.Equal(0, new Yatzy(6, 2, 2, 4, 5).GetScoreOnes(6, 2, 2, 4, 5));
+            Assert.Equal(4, new Yatzy(1, 2, 1, 1, 1).GetScoreOnes(1, 2, 1, 1, 1));
         }
 
         [Fact]
         public void Fact_2s()
         {
-            Assert.Equal(4, Yatzy.GetScoreTwos(1, 2, 3, 2, 6));
-            Assert.Equal(10, Yatzy.GetScoreTwos(2, 2, 2, 2, 2));
+            Assert.Equal(4, new Yatzy(1, 2, 3, 2, 6).GetScoreTwos(1, 2, 3, 2, 6));
+            Assert.Equal(10, new Yatzy(2, 2, 2, 2, 2).GetScoreTwos(2, 2, 2, 2, 2));
         }
 
         [Fact]
         public void Fact_threes()
         {
-            Assert.Equal(6, Yatzy.GetScoreThrees(1, 2, 3, 2, 3));
-            Assert.Equal(12, Yatzy.GetScoreThrees(2, 3, 3, 3, 3));
+            Assert.Equal(6, new Yatzy(1, 2, 3, 2, 3).GetScoreThrees(1, 2, 3, 2, 3));
+            Assert.Equal(12, new Yatzy(2, 3, 3, 3, 3).GetScoreThrees(2, 3, 3, 3, 3));
         }
 
         [Fact]
@@ -47,9 +47,9 @@ namespace Yatzy.Test
         [Fact]
         public void four_of_a_knd()
         {
-            Assert.Equal(12, Yatzy.GetScoreFourOfAKind(3, 3, 3, 3, 5));
-            Assert.Equal(20, Yatzy.GetScoreFourOfAKind(5, 5, 5, 4, 5));
-            Assert.Equal(12, Yatzy.GetScoreFourOfAKind(3, 3, 3, 3, 3));
+            Assert.Equal(12, new Yatzy(3, 3, 3, 3, 5).GetScoreFourOfAKind(3, 3, 3, 3, 5));
+            Assert.Equal(20, new Yatzy(5, 5, 5, 4, 5).GetScoreFourOfAKind(5, 5, 5, 4, 5));
+            Assert.Equal(12, new Yatzy(3, 3, 3, 3, 3).GetScoreFourOfAKind(3, 3, 3, 3, 3));
         }
 
         [Fact]
@@ -63,24 +63,24 @@ namespace Yatzy.Test
         [Fact]
         public void fullHouse()
         {
-            Assert.Equal(18, Yatzy.GetScoreFullHouse(6, 2, 2, 2, 6));
-            Assert.Equal(0, Yatzy.GetScoreFullHouse(2, 3, 4, 5, 6));
+            Assert.Equal(18, new Yatzy(6, 2, 2, 2, 6).GetScoreFullHouse(6, 2, 2, 2, 6));
+            Assert.Equal(0, new Yatzy(2, 3, 4, 5, 6).GetScoreFullHouse(2, 3, 4, 5, 6));
         }
 
         [Fact]
         public void largeStraight()
         {
-            Assert.Equal(20, Yatzy.GetScoreLargeStraight(6, 2, 3, 4, 5));
-            Assert.Equal(20, Yatzy.GetScoreLargeStraight(2, 3, 4, 5, 6));
-            Assert.Equal(0, Yatzy.GetScoreLargeStraight(1, 2, 2, 4, 5));
+            Assert.Equal(20, new Yatzy(6, 2, 3, 4, 5).GetScoreLargeStraight(6, 2, 3, 4, 5));
+            Assert.Equal(20, new Yatzy(2, 3, 4, 5, 6).GetScoreLargeStraight(2, 3, 4, 5, 6));
+            Assert.Equal(0, new Yatzy(1, 2, 2, 4, 5).GetScoreLargeStraight(1, 2, 2, 4, 5));
         }
 
         [Fact]
         public void one_pair()
         {
-            Assert.Equal(6, Yatzy.GetScoreOnePair(3, 4, 3, 5, 6));
-            Assert.Equal(10, Yatzy.GetScoreOnePair(5, 3, 3, 3, 5));
-            Assert.Equal(12, Yatzy.GetScoreOnePair(5, 3, 6, 6, 5));
+            Assert.Equal(6, new Yatzy(3, 4, 3, 5, 6).GetScoreOnePair(3, 4, 3, 5, 6));
+            Assert.Equal(10, new Yatzy(5, 3, 3, 3, 5).GetScoreOnePair(5, 3, 3, 3, 5));
+            Assert.Equal(12, new Yatzy(5, 3, 6, 6, 5).GetScoreOnePair(5, 3, 6, 6, 5));
         }
 
         [Fact]
@@ -94,34 +94,34 @@ namespace Yatzy.Test
         [Fact]
         public void smallStraight()
         {
-            Assert.Equal(15, Yatzy.GetScoreSmallStraight(1, 2, 3, 4, 5));
-            Assert.Equal(15, Yatzy.GetScoreSmallStraight(2, 3, 4, 5, 1));
-            Assert.Equal(0, Yatzy.GetScoreSmallStraight(1, 2, 2, 4, 5));
+            Assert.Equal(15, new Yatzy(1, 2, 3, 4, 5).GetScoreSmallStraight(1, 2, 3, 4, 5));
+            Assert.Equal(15, new Yatzy(2, 3, 4, 5, 1).GetScoreSmallStraight(2, 3, 4, 5, 1));
+            Assert.Equal(0, new Yatzy(1, 2, 2, 4, 5).GetScoreSmallStraight(1, 2, 2, 4, 5));
         }
 
         [Fact]
         public void three_of_a_kind()
         {
-            Assert.Equal(9, Yatzy.GetScoreThreeOfAKind(3, 3, 3, 4, 5));
-            Assert.Equal(15, Yatzy.GetScoreThreeOfAKind(5, 3, 5, 4, 5));
-            Assert.Equal(9, Yatzy.GetScoreThreeOfAKind(3, 3, 3, 3, 5));
+            Assert.Equal(9, new Yatzy(3, 3, 3, 4, 5).GetScoreThreeOfAKind(3, 3, 3, 4, 5));
+            Assert.Equal(15, new Yatzy(5, 3, 5, 4, 5).GetScoreThreeOfAKind(5, 3, 5, 4, 5));
+            Assert.Equal(9, new Yatzy(3, 3, 3, 3, 5).GetScoreThreeOfAKind(3, 3, 3, 3, 5));
         }
 
         [Fact]
         public void two_Pair()
         {
-            Assert.Equal(16, Yatzy.GetScoreTwoPairs(3, 3, 5, 4, 5));
-            Assert.Equal(16, Yatzy.GetScoreTwoPairs(3, 3, 5, 5, 5));
+            Assert.Equal(16, new Yatzy(3, 3, 5, 4, 5).GetScoreTwoPairs(3, 3, 5, 4, 5));
+            Assert.Equal(16, new Yatzy(3, 3, 5, 5, 5).GetScoreTwoPairs(3, 3, 5, 5, 5));
         }
 
         [Fact]
         public void Yatzy_scores_50()
         {
             var expected = 50;
-            var actual = Yatzy.GetScoreYams(4, 4, 4, 4, 4);
+            var actual = new Yatzy(4, 4, 4, 4, 4).GetScoreYams(4, 4, 4, 4, 4);
             Assert.Equal(expected, actual);
-            Assert.Equal(50, Yatzy.GetScoreYams(6, 6, 6, 6, 6));
-            Assert.Equal(0, Yatzy.GetScoreYams(6, 6, 6, 6, 3));
+            Assert.Equal(50, new Yatzy(6, 6, 6, 6, 6).GetScoreYams(6, 6, 6, 6, 6));
+            Assert.Equal(0, new Yatzy(6, 6, 6, 6, 3).GetScoreYams(6, 6, 6, 6, 3));
         }
     }
 }
--- csharp/Yatzy/Yatzy.cs
+++ csharp/Yatzy/Yatzy.cs
@@ -12,34 +12,32 @@ namespace Yatzy
             _dice = new[] {d1, d2, d3, d4, d5};
         }
 
-        public static int GetScoreChance(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreChance(int d1, int d2, int d3, int d4, int d5)
         {
             var dice = CreateDiceArray(d1, d2, d3, d4, d5);
             return dice.Sum();
         }
 
-        public static int GetScoreYams(params int[] dice)
+        public int GetScoreYams(params int[] dice)
         {
             var numberDiceWithValue = CountDiceOccurrences(dice);
             foreach (var count in numberDiceWithValue)
-            {
                 if (count == 5)
                     return 50;
-            }
             return 0;
         }
 
-        public static int GetScoreOnes(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreOnes(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreForValue(d1, d2, d3, d4, d5, 1);
         }
 
-        public static int GetScoreTwos(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreTwos(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreForValue(d1, d2, d3, d4, d5, 2);
         }
 
-        public static int GetScoreThrees(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreThrees(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreForValue(d1, d2, d3, d4, d5, 3);
         }
@@ -59,12 +57,12 @@ namespace Yatzy
             return GetScoreForValue(_dice[0], _dice[1], _dice[2], _dice[3], _dice[4], 6);
         }
 
-        public static int GetScoreOnePair(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreOnePair(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreGroupsOf(d1, d2, d3, d4, d5, 2, 1);
         }
 
-        public static int GetScoreTwoPairs(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreTwoPairs(int d1, int d2, int d3, int d4, int d5)
         {
             if (IsApplicable(d1, d2, d3, d4, d5, 2, 2))
                 return GetScoreGroupsOf(d1, d2, d3, d4, d5, 2, 2);
@@ -72,31 +70,31 @@ namespace Yatzy
             return 0;
         }
 
-        public static int GetScoreFourOfAKind(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreFourOfAKind(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreGroupsOf(d1, d2, d3, d4, d5, 4, 1);
         }
 
-        public static int GetScoreThreeOfAKind(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreThreeOfAKind(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreGroupsOf(d1, d2, d3, d4, d5, 3, 1);
         }
 
-        public static int GetScoreSmallStraight(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreSmallStraight(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreStraight(d1, d2, d3, d4, d5, 1, 5);
         }
 
-        public static int GetScoreLargeStraight(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreLargeStraight(int d1, int d2, int d3, int d4, int d5)
         {
             return GetScoreStraight(d1, d2, d3, d4, d5, 2, 6);
         }
 
-        public static int GetScoreFullHouse(int d1, int d2, int d3, int d4, int d5)
+        public int GetScoreFullHouse(int d1, int d2, int d3, int d4, int d5)
         {
             if (IsApplicable(d1, d2, d3, d4, d5, 2, 1)
                 && IsApplicable(d1, d2, d3, d4, d5, 3, 1)
-                ) 
+            )
                 return GetScoreOnePair(d1, d2, d3, d4, d5) + GetScoreThreeOfAKind(d1, d2, d3, d4, d5);
 
             return 0;
@@ -137,7 +135,7 @@ namespace Yatzy
         */
         }
 
-        private static int[] CountDiceOccurrences(int[] dice)
+        private int[] CountDiceOccurrences(int[] dice)
         {
             var counts = new int[7];
             foreach (var die in dice)
@@ -145,31 +143,29 @@ namespace Yatzy
             return counts;
         }
 
-        private static int[] CreateDiceArray(int d1, int d2, int d3, int d4, int d5)
+        private int[] CreateDiceArray(int d1, int d2, int d3, int d4, int d5)
         {
             var dice = new[] {d1, d2, d3, d4, d5};
             return dice;
         }
 
-        private static int CountDiceWithValue(int[] dice, int value)
+        private int CountDiceWithValue(int[] dice, int value)
         {
             var count = 0;
             foreach (var die in dice)
-            {
                 if (die == value)
                     count++;
-            }
 
             return count;
         }
 
-        private static int GetScoreForValue(int d1, int d2, int d3, int d4, int d5, int dieValue)
+        private int GetScoreForValue(int d1, int d2, int d3, int d4, int d5, int dieValue)
         {
             var dice = CreateDiceArray(d1, d2, d3, d4, d5);
             return CountDiceWithValue(dice, dieValue) * dieValue;
         }
 
-        private static int GetScoreGroupsOf(int d1, int d2, int d3, int d4, int d5,
+        private int GetScoreGroupsOf(int d1, int d2, int d3, int d4, int d5,
             int numberElementsInGroup, int numberGroups)
         {
             var dice = CreateDiceArray(d1, d2, d3, d4, d5);
@@ -177,28 +173,25 @@ namespace Yatzy
             return GetScoreGroupsOf(numberDiceWithValue, numberElementsInGroup, numberGroups);
         }
 
-        private static int GetScoreGroupsOf(int[] numberDiceWithValue, int numberElementsInGroup, int numberGroups)
+        private int GetScoreGroupsOf(int[] numberDiceWithValue, int numberElementsInGroup, int numberGroups)
         {
             var pairs = GetDescendingGroupsOf(numberDiceWithValue, numberElementsInGroup);
             return pairs.Take(numberGroups).Sum(x => numberElementsInGroup * x);
         }
 
-        private static List<int> GetDescendingGroupsOf(int[] numberDiceWithValue, int numberElementsInGroup)
+        private List<int> GetDescendingGroupsOf(int[] numberDiceWithValue, int numberElementsInGroup)
         {
             var groups = new List<int>();
 
             for (var dieValue = 6; dieValue >= 1; dieValue--)
-            {
                 if (numberDiceWithValue[dieValue] >= numberElementsInGroup)
-                {
                     groups.Add(dieValue);
-                }
-            }
 
             return groups;
         }
 
-        private static int GetScoreStraight(int d1, int d2, int d3, int d4, int d5, int lowerDieValue, int higherDieValue)
+        private int GetScoreStraight(int d1, int d2, int d3, int d4, int d5, int lowerDieValue,
+            int higherDieValue)
         {
             var dice = CreateDiceArray(d1, d2, d3, d4, d5);
             var numberDiceWithValue = CountDiceOccurrences(dice);
@@ -215,13 +208,15 @@ namespace Yatzy
             return sum;
         }
 
-        private static bool IsApplicable(int d1, int d2, int d3, int d4, int d5, int numberElementsInGroup, int minimumNumberGroups)
+        private bool IsApplicable(int d1, int d2, int d3, int d4, int d5, int numberElementsInGroup,
+            int minimumNumberGroups)
         {
             var groupsByDescendingValue = GetDescendingGroupsOf(d1, d2, d3, d4, d5, numberElementsInGroup);
             return groupsByDescendingValue.Count() >= minimumNumberGroups;
         }
 
-        private static List<int> GetDescendingGroupsOf(int d1, int d2, int d3, int d4, int d5, int numberElementsInGroup)
+        private List<int> GetDescendingGroupsOf(int d1, int d2, int d3, int d4, int d5,
+            int numberElementsInGroup)
         {
             var dice = CreateDiceArray(d1, d2, d3, d4, d5);
             var numberDiceWithValue = CountDiceOccurrences(dice);
